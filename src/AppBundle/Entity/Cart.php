715809d04bb580d3933e186631d33c9118c5e286
Jan Suchánek <jan.suchanek@gmail.com>
2016-11-08 00:33:14 +0100

    Add Cart and CartItem Entities

---
+++ src/AppBundle/Entity/Cart.php
@@ -0,0 +1,64 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @author Vašek Boch <vasek.boch@live.com>
+ * @author Jan Klat <jenik@klatys.cz>
+ *
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CartRepository")
+ */
+class Cart
+{
+
+	/**
+	 * @var int
+	 * @ORM\Id
+	 * @ORM\GeneratedValue
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
+
+	/**
+	 * @var User
+	 * @ORM\ManyToOne(targetEntity="User")
+	 */
+	private $user;
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @param int $id
+	 * @return self
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+		return $this;
+	}
+
+	/**
+	 * @return User
+	 */
+	public function getUser()
+	{
+		return $this->user;
+	}
+
+	/**
+	 * @param User $user
+	 * @return self
+	 */
+	public function setUser($user)
+	{
+		$this->user = $user;
+		return $this;
+	}
+}
---
+++ src/AppBundle/Entity/CartItem.php
@@ -0,0 +1,136 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @author Vašek Boch <vasek.boch@live.com>
+ * @author Jan Klat <jenik@klatys.cz>
+ *
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CartItemRepository")
+ */
+class Cart
+{
+
+	/**
+	 * @var int
+	 * @ORM\Id
+	 * @ORM\GeneratedValue
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
+
+	/**
+	 * @var Cart
+	 * @ORM\ManyToOne(targetEntity="Cart")
+	 */
+	private $cart;
+
+	/**
+	 * @var Product
+	 * @ORM\OneToMany(targetEntity="Product")
+	 */
+	private $product;
+
+	/**
+	 * @var int
+	 * @ORM\Column(type="integer")
+	 */
+	private $quantity;
+
+	/**
+	 * @var int
+	 * @ORM\Column(type="integer")
+	 */
+	private $pricePerItem;
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @param int $id
+	 * @return self
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+		return $this;
+	}
+
+	/**
+	 * @return Cart
+	 */
+	public function getCart()
+	{
+		return $this->cart;
+	}
+
+	/**
+	 * @param Cart $cart
+	 * @return self
+	 */
+	public function setCart($cart)
+	{
+		$this->cart = $cart;
+		return $this;
+	}
+
+	/**
+	 * @return Product
+	 */
+	public function getProduct()
+	{
+		return $this->product;
+	}
+
+	/**
+	 * @param Product $product
+	 * @return self
+	 */
+	public function setProduct($product)
+	{
+		$this->product = $product;
+		return $this;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getQuantity()
+	{
+		return $this->quantity;
+	}
+
+	/**
+	 * @param int $quantity
+	 * @return self
+	 */
+	public function setQuantity($quantity)
+	{
+		$this->quantity = $quantity;
+		return $this;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getPricePerItem()
+	{
+		return $this->pricePerItem;
+	}
+
+	/**
+	 * @param int $pricePerItem
+	 * @return self
+	 */
+	public function setPricePerItem($pricePerItem)
+	{
+		$this->pricePerItem = $pricePerItem;
+		return $this;
+	}
+}
