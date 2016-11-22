4d1596372e260b0afdbbc072a0e2b15c34404f65
Jan Suchánek <jan.suchanek@gmail.com>
2016-11-22 14:27:26 +0100

    Add Form for Product detail

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -1,12 +1,16 @@
 <?php
-
 namespace AppBundle\Controller;
 
 use AppBundle\Facade\ProductFacade;
 use AppBundle\Facade\WarehouseProductFacade;
+use AppBundle\FormType\CartAddFormType;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
 
 
 /**
@@ -18,17 +22,29 @@ class ProductController
 {
 	private $productFacade;
 	private $warehouseProductFacade;
+	private $formFactory;
+	private $router;
 
-	public function __construct(ProductFacade $productFacade, WarehouseProductFacade $warehouseProductFacade)
+	public function __construct(
+		ProductFacade $productFacade,
+		WarehouseProductFacade $warehouseProductFacade,
+		FormFactory $formFactory,
+		RouterInterface $router
+	)
 	{
 		$this->productFacade = $productFacade;
 		$this->warehouseProductFacade = $warehouseProductFacade;
+		$this->formFactory = $formFactory;
+		$this->router = $router;
 	}
 	/**
 	 * @Route("/product/{slug}", name="product_detail")
 	 * @Template("product/detail.html.twig")
+	 *
+	 * @param Request $request
+	 * @return RedirectResponse|array
 	 */
-	public function productDetailAction($slug)
+	public function productDetailAction(Request $request, $slug)
 	{
 		$product = $this->productFacade->getBySlug($slug);
 		if (!$product) {
@@ -39,9 +55,18 @@ class ProductController
 
 		$quantity = $this->warehouseProductFacade->getQuantityByProduct($product);
 
+		$form = $this->formFactory->create(CartAddFormType::class, []);
+
+		$form->handleRequest($request);
+		if ($form->isSubmitted() && $form->isValid()) {
+
+			return RedirectResponse::create($this->router->generate("cart_detail"));
+		}
+
 		return [
 			"product" => $product,
 			"warehouseProducts" => $warehouseProducts,
+			"form" => $form->createView(),
 		];
 	}
 
